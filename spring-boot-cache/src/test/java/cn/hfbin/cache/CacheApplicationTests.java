8a6825cffc5a4be2fb85db17f101ad036f0af86e
hfbin <1716850375@qq.com>
2018-06-05 19:04:01 +0800

    Cache

--- spring-boot-cache/src/test/java/cn/hfbin/cache/CacheApplicationTests.java
+++ spring-boot-cache/src/test/java/cn/hfbin/cache/CacheApplicationTests.java
@@ -1,16 +1,25 @@
 package cn.hfbin.cache;
 
+import cn.hfbin.cache.mapper.EmployeeMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class CacheApplicationTests {
+
+	@Autowired
+	EmployeeMapper employeeMapper;
 
 	@Test
 	public void contextLoads() {
+		log.info(employeeMapper.getEmployeeById(1)+"");
+		System.out.println();
 	}
 
 }
