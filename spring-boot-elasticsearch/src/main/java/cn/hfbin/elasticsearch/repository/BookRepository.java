a1f3557b2d9c66e2e4fb866f8d0878ec93f66f8e
hfbin <1716850375@qq.com>
2018-06-07 19:22:42 +0800

    elasticsearch

--- spring-boot-elasticsearch/src/main/java/cn/hfbin/elasticsearch/repository/BookRepository.java
+++ spring-boot-elasticsearch/src/main/java/cn/hfbin/elasticsearch/repository/BookRepository.java
@@ -15,6 +15,9 @@ import java.util.List;
 
 public  interface BookRepository extends ElasticsearchRepository<Book , Integer> {
 
+    //具体写法参考官方的
+    //https://docs.spring.io/spring-data/elasticsearch/docs/3.0.7.RELEASE/reference/html/#elasticsearch.misc.filter
+
     public List<Book> findBooksByBookContent(String string);
 
     public List<Book> findBooksByBookContentLike(String string);
